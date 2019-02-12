d6ad64d2374e9d1410fea77485bb78a39c7c5944
Olexiy665 <alexey6mk@gmail.com>
2019-02-12 17:10:23 +0200

    Updates

---
+++ precache-manifest.9ca4aa0cfd0d86b8e37664adb2a84de2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8673388ce1616c3ad8f36e79212e6040",
+    "url": "/react-filter-demo/static/media/delete.8673388c.svg"
+  },
+  {
+    "revision": "13172ef4b7545a363db2c35e4a41b348",
+    "url": "/react-filter-demo/static/media/arrow.13172ef4.svg"
+  },
+  {
+    "revision": "3facec65cc9f54387b79",
+    "url": "/react-filter-demo/static/js/runtime~main.3facec65.js"
+  },
+  {
+    "revision": "2a7a3f2a445ad064cf87",
+    "url": "/react-filter-demo/static/js/main.2a7a3f2a.chunk.js"
+  },
+  {
+    "revision": "c4340537b8816827970c",
+    "url": "/react-filter-demo/static/js/1.c4340537.chunk.js"
+  },
+  {
+    "revision": "2a7a3f2a445ad064cf87",
+    "url": "/react-filter-demo/static/css/main.161634b0.chunk.css"
+  },
+  {
+    "revision": "24484c7948a38ec26aafa378d8b0533f",
+    "url": "/react-filter-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/github.io/react-filter-demo/precache-manifest.330a1a7eb5e2ba9a0275c681702172e4.js"
+  "/react-filter-demo/precache-manifest.9ca4aa0cfd0d86b8e37664adb2a84de2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/github.io/react-filter-demo/index.html", {
+workbox.routing.registerNavigationRoute("/react-filter-demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
